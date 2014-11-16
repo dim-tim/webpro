0185c7411d285b72dd95dab8823872428e5455bd
Dzmitry Kislo <Dzmitry_Kislo@epam.com>
2014-11-16 17:31:37 +0300

    Create new project with ant

---
+++ src/com/kd/pack/TestClass.java
@@ -0,0 +1,10 @@
+package com.kd.pack;
+
+/**
+ * Created by dima on 15.11.14.
+ */
+public class TestClass {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
